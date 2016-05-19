7d23db451607ac2a1f559d2677f128a73f4faad2
RyanAbraham <ryan_ja@live.ca>
2016-05-19 14:16:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <!-- Style tags below are CSS for the sidebar-->
 <style>
+<!-- I don't actually know clearfix is for but it might fix something -->
 .clearFix {
     clear: both;
 }
@@ -15,22 +16,29 @@
 }
 </style>
 
+
+
+
+
 <!-- Website HTML begins here -->
 <form action="index.php">
-	<DIV style="position:absolute; top:200px; left:200px;">Home page</DIV>
 	<center><input type="image" src="cw_logo.png" alt="Home Page"></center><br><br><br>
 </form>
 
 <div id="sidebar">
-<form action="create_page.php">
+<form action="create_page.html">
 	<center><br><input type="submit" value="Create New Page"></center>
 </form>
 </div>
 <div align="right">
 <form action="search.php" method="GET">
-	<input type="text" name="search_term">
+	<input type="text" style="width: 300px;" name="search_term">
 	<input type="submit" value="Search">
 </form>
+</div>
+
+<!-- Page content starts here -->
+<div style="position:relative; left:20px;">This is the home page</div>
 
 </html>
 </body>
