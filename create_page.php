2b5ddace19ba0446ad31e48e7cc591757062526a
JacksonSchoe <jacksonschoenermarck@cmail.carleton.ca>
2016-05-20 11:56:16 -0400

    duplicate title fix

--- create_page.php
+++ create_page.php
@@ -1,6 +1,32 @@
 <html>
 <body>
-<?php include("header.html"); ?>
+<?php include("header.html"); 
+
+//Checks to see if the same title is already in use and if it is display the appropriate error message 
+//and prompt the user to go to the page with the title they've inputted 
+if(isset($_GET['same'])){
+	if($_GET['same'] == 1){
+		$title = $_GET['title'];
+		
+		$coloruserf = 'red';
+		echo '<p><font color="red">The title you have have requested is already in use. Visit the page here:</font></p>';
+		?>
+		
+		<form action="page.php">
+		<!-- The hidden field is needed to be able to reference whatever page the user created -->
+		<input type="hidden" name="page" value=<?php echo "$title"; ?>>
+		<input type="submit" value="Go to your page">
+		</form>
+		
+		
+		<?php
+		
+		
+	}
+}
+
+?>
+
 
 <!-- Page with two text fields to create a new wiki page -->
 <form action = "create_page2.php" method = "POST">
