33bb65a8b1bc34098e0147f1ca1183f2099eb780
RyanAbraham <ryan_ja@live.ca>
2016-05-20 10:27:28 -0400

    Add files via upload

--- create_page.php
+++ create_page.php
@@ -4,11 +4,10 @@
 
 <!-- Page with two text fields to create a new wiki page -->
 <form action = "create_page2.php" method = "POST">
-<b>Title for your page:<br></b> <input type = "text" name ="title"> <br><br>
-<b>Text about your page:<br></b> <textarea name = "body" rows="10" cols="40"></textarea> <br>
+<p><input type="submit" value="Create Page"></p>
+<b>Title for your page:<br></b> <input type = "text" name ="title" placeholder="Title" required="required"> <br><br>
+<b>Text about your page:<br></b> <textarea name = "body" rows="10" cols="40" placeholder="Text" required="required"></textarea> <br>
 </div>
-<div style="position:absolute; left:180px;">
-<input type="submit" value="Create Page">
 </form>
 
 </body>
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <body>
 <?php include("header.html"); ?>
 
-<!-- Page content starts here -->
-<div style="position:relative; left:20px;">This is the home page</div>
+<!-- Home page -->
+This is the home page
 
 </html>
 </body>
